eb0892e6b6bd9e3b3bb6bd52a0faf85740b40d2a
seIcouth <tmz.emre@outlook.com>
2023-04-06 16:35:17 +0300

    notifications added

--- lib/screens/auth_screens/login.dart
+++ lib/screens/auth_screens/login.dart
@@ -14,20 +14,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   TextEditingController mail = TextEditingController();
   TextEditingController pass = TextEditingController();
 
-  Future<List> login() async{
-    final response = await http.post(Uri.parse("http://10.0.2.2/graded/login.php"), body: {
-      "mail": mail.text,
-      "password": pass.text
-    });
+  Future<List> login() async {
+    final response = await http.post(
+        Uri.parse("http://10.0.2.2/graded/login.php"),
+        body: {"mail": mail.text, "password": pass.text});
 
     List<dynamic> datauser = json.decode(response.body);
 
-    if(datauser.isNotEmpty){
-      if(datauser[0]['role']=='student'){
+    if (datauser.isNotEmpty) {
+      if (datauser[0]['role'] == 'student') {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setBool('isLoggedIn', true);
         Navigator.of(context).push(
@@ -35,7 +33,7 @@ class _LoginPageState extends State<LoginPage> {
             builder: (context) => const HomePage(),
           ),
         );
-      } else if(datauser[0]['role']=='instructor'){
+      } else if (datauser[0]['role'] == 'instructor') {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setBool('isLoggedIn', true);
         Navigator.of(context).push(
@@ -44,7 +42,7 @@ class _LoginPageState extends State<LoginPage> {
           ),
         );
       }
-    }else{
+    } else {
       throw const FormatException('Expected at least 1 section');
     }
 
@@ -58,196 +56,205 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: const Color(0xfffff4f0),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                height: 300,
-                child: Image.asset('assets/images/logo_straight.png'),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 30.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Login to your GRADED account',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff0e1e40),
-                    ),
-                  ),
+    return Scaffold(
+      backgroundColor: const Color(0xfffff4f0),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 300,
+                  child: Image.asset('assets/images/logo_straight.png'),
                 ),
-              ),
-              const SizedBox(height: 18,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: TextField(
-                  cursorHeight: 16,
-                  cursorWidth: 1.5,
-                  cursorColor: const Color(0xff0e1e40),
-                  controller: mail,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xff0e1e40)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Email',
-                    fillColor: Colors.grey[100],
-                    filled: true,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: TextField(
-                  obscureText: true,
-                  cursorHeight: 16,
-                  cursorWidth: 1.5,
-                  cursorColor: const Color(0xff0e1e40),
-                  controller: pass,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xff0e1e40)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[100],
-                    filled: true,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 30.0),
-                child: Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: (){
-                        //TODO: Forgot password screen implementation
-                        /*
-                        Navigator.push(context, MaterialPageRoute(builder: (context){}));
-                         */
-                      },
-                      child: const Text(
-                          'Forgot password?',
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold
-                          )
+                const Padding(
+                  padding: EdgeInsets.only(left: 30.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Login to your GRADED account',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff0e1e40),
                       ),
-                    )
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if(mail.text.isEmpty || pass.text.isEmpty){
-                      Flushbar(
-                        message: "Please fill all necessary fields.",
-                        duration: const Duration(seconds: 3),
-                        margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 50),
-                      ).show(context);
-                    } else{
-                      if(isValidEmail(mail.text)){
-                        try{
-                          await login();
-                        } on FormatException catch(e){
+                const SizedBox(
+                  height: 18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    cursorHeight: 16,
+                    cursorWidth: 1.5,
+                    cursorColor: const Color(0xff0e1e40),
+                    controller: mail,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xff0e1e40)),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Email',
+                      fillColor: Colors.grey[100],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    obscureText: true,
+                    cursorHeight: 16,
+                    cursorWidth: 1.5,
+                    cursorColor: const Color(0xff0e1e40),
+                    controller: pass,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xff0e1e40)),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Password',
+                      fillColor: Colors.grey[100],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0),
+                  child: Align(
+                      alignment: Alignment.centerRight,
+                      child: GestureDetector(
+                        onTap: () {
+                          //TODO: Forgot password screen implementation
+                          /*
+                          Navigator.push(context, MaterialPageRoute(builder: (context){}));
+                           */
+                        },
+                        child: const Text('Forgot password?',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                      )),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (mail.text.isEmpty || pass.text.isEmpty) {
+                        Flushbar(
+                          message: "Please fill all necessary fields.",
+                          duration: const Duration(seconds: 3),
+                          margin: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom +
+                                  50),
+                        ).show(context);
+                      } else {
+                        if (isValidEmail(mail.text)) {
+                          try {
+                            await login();
+                          } on FormatException catch (e) {
+                            Flushbar(
+                              message:
+                                  "Something went wrong, please try again.",
+                              duration: const Duration(seconds: 3),
+                              margin: EdgeInsets.only(
+                                  bottom:
+                                      MediaQuery.of(context).viewInsets.bottom +
+                                          50),
+                            ).show(context);
+                          }
+                        } else {
                           Flushbar(
-                            message: "Something went wrong, please try again.",
+                            message: "The email address is badly formatted.",
                             duration: const Duration(seconds: 3),
-                            margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 50),
+                            margin: EdgeInsets.only(
+                                bottom:
+                                    MediaQuery.of(context).viewInsets.bottom +
+                                        50),
                           ).show(context);
                         }
-                      } else{
-                        Flushbar(
-                          message: "The email address is badly formatted.",
-                          duration: const Duration(seconds: 3),
-                          margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 50),
-                        ).show(context);
                       }
-                    }
-
-                  },
-                   style: ButtonStyle(
-                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                           RoundedRectangleBorder(
-                               borderRadius: BorderRadius.circular(12.0),
-                           )
-                       ),
-                     backgroundColor: MaterialStateProperty.all(const Color(0xff0e1e40)),
-                   ),
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    child: const Center(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      )),
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xff0e1e40)),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      child: const Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'New to Graded?',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff0e1e40),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      ' Register now',
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'New to Graded?',
                       style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff0e1e40),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        ' Register now',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -2,26 +2,241 @@ import 'package:flutter/material.dart';
 import 'package:graded/screens/auth_screens/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _index = 0;
+
+  final Color colorLight = const Color(0xfffff4f0);
+  final Color colorDark = const Color(0xff0e1e40);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            SharedPreferences prefs = await SharedPreferences.getInstance();
-            await prefs.setBool('isLoggedIn', false);
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ),
-            );
-          },
-          child: const Text('Log out'),
+      backgroundColor: colorLight,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: colorLight,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: colorDark,
+        leading: Icon(
+          Icons.menu_rounded,
+          color: colorLight,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: colorLight,
+                  elevation: 2.0,
+                  shadowColor: colorDark,
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.notifications_none_outlined,
+                                  color: colorDark,
+                                ),
+                                Text(
+                                  "Recent Notifications",
+                                  style: TextStyle(
+                                    color: colorDark,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 170, // card height
+                          child: PageView.builder(
+                            itemCount: 5,
+                            controller: PageController(viewportFraction: 0.7),
+                            onPageChanged: (int index) =>
+                                setState(() => _index = index),
+                            itemBuilder: (_, i) {
+                              return Transform.scale(
+                                scale: i == _index ? 1 : 0.85,
+                                child: Card(
+                                  color: Colors.teal,
+                                  elevation: 6,
+                                  shadowColor: colorDark,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: announcementNotificationCard(
+                                      context,
+                                      "Announcement 1",
+                                      "sedrfgthyujıkolpşlokıujhygtfrtgyhujıkolpşlokıujhygtfrtgyhujıkolpşlokıuytyuıop",
+                                      "COMP204",
+                                      "Database Management Systems",
+                                      Colors.teal),
+                                  /*
+                                  Center(
+                                    child: Text(
+                                      "Card ${i + 1}",
+                                      style: TextStyle(
+                                        fontSize: 32,
+                                        color: colorLight,
+                                      ),
+                                    ),
+                                  ),
+                                  */
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    await prefs.setBool('isLoggedIn', false);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Log out'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  Widget announcementNotificationCard(BuildContext context, String title,
+      String content, String courseCode, String courseName, Color color) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      color: colorLight,
+      elevation: 2.0,
+      child: Container(
+        margin: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.announcement_rounded,
+                              color: color,
+                            ),
+                            Expanded(
+                              child: Text(
+                                title,
+                                style: TextStyle(
+                                  color: color,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                softWrap: false,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        const Divider(
+                          color: Colors.grey, //color of divider
+                          height: 5, //height spacing of divider
+                          thickness: 1.5, //thickness of divider line
+                          indent: 5, //spacing at the start of divider
+                          endIndent: 5, //spacing at the end of divider
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          content,
+                          style: TextStyle(
+                            color: color,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          softWrap: false,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 2.0),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "$courseCode • $courseName",
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  softWrap: false,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget assignmentNotificationCard(
+      BuildContext context, String title, String courseCode) {
+    return Container();
   }
 }
