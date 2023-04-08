de77adaf28d5fdc03353120e6ab7e7118a76b90d
yagizerdm <yagizeerdem@gmail.com>
2023-04-08 18:46:55 +0300

    reusable methods

---
+++ lib/resources/reusable_methods.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ReusableMethods{
+  static Color colorDark = const Color(0xff0e1e40);
+  static Color colorLight = const Color(0xfffff4f0);
+  static Color colorBlue = const Color(0xff023e8a);
+
+  static bool isValidEmail(String email) {
+    RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return emailRegExp.hasMatch(email);
+  }
+
+  static Future<bool> isLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+    return isLoggedIn;
+  }
+
+  static Future<void> setLoggedInTrue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', true);
+  }
+
+  static Future<void> setLoggedInFalse() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', false);
+  }
+
+}
